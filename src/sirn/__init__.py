1e0dbb9f0bb913576fdd691d1fa0dd96eaa8e873
Joseph Hellerstein <jlheller@uw.edu>
2024-11-29 21:10:57 -0800

    Initial progress on API.

--- src/sirn/__init__.py
+++ src/sirn/__init__.py
@@ -6,4 +6,16 @@ A Network is a representation of a chemical reaction network that includes a Nam
 A Constraint describes numerical or categorical charateristics of a network that are organized as one or more NamedMatrix.
 """
 import sirn.constants as cn  # type: ignore
-from sirn.api import findForDirectories, findForModels, serialize  # type: ignore
+
+
+def findForDirectories(*args, **kwargs):
+    from sirn.api import findForDirectories
+    return findForDirectories(*args, **kwargs)
+
+def findForModels(*args, **kwargs):
+    from sirn.api import findForModels
+    return findForModels(*args, **kwargs)
+
+def serialize(*args, **kwargs):
+    from sirn.api import serialize
+    return serialize(*args, **kwargs)
