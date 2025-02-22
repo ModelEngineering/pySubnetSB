7a18d90b7c59be49b8623a3e6212ff462fde8acd
Joseph Hellerstein <jlheller@uw.edu>
2025-02-21 18:48:33 -0800

    Fix workflow 11

--- activate.sh
+++ activate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source ../BaseStack/bin/setup_run.sh
-PYTHONPATH=`pwd`/src:${PYTHONPATH}
+PYTHONPATH=`pwd`/src:`pwd`/analysis_src:${PYTHONPATH}
 export PYTHONPATH
 source sub/bin/activate
